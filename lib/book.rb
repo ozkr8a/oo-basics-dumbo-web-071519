69050664ca2c9be0edd13d4f56aee07c3d23d213
Oscar <oscar_ochoac@yahoo.com>
2019-07-17 20:19:23 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,42 @@
 
+class Book
+  
+  attr_reader
+  attr_writer
+  
+  def initialize(title)
+    
+    @title = title
+    
+  end
+  
+  def title
+    @title
+  end
+  
+  def author
+    @author
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
+  
+end
 
